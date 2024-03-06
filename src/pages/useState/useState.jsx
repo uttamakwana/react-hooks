dba348484480c9f36c3741318588f5a1a0e5ff54
Uttam Makwana <uttamakwana4503@gmail.com>
2024-03-06 11:44:28 +0530

    Hook: useEffect is completed

--- src/pages/useState/useState.jsx
+++ src/pages/useState/useState.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 
 const UseState = () => {
-  const [counter, setCounter] = useState(0);
+  // const [value, setValue] = useState(initialValue);
+  // const [counter, setCounter] = useState(0);
+  //! initialValue = 0 will be passed down on each render
+  // you can pass initialValue like the below for better performance
 
+  const [counter, setCounter] = useState(() => 0);
+  //* Now initialValue will be passed down only on the first render.
+
+  //does: Increment count by one
   function incrementCounter() {
     setCounter((prev) => prev + 1);
   }
 
+  //does: Decrement count by one (only if counter is greater then 0)
   function decrementCounter() {
     if (counter > 0) {
       setCounter((prev) => prev - 1);
     }
   }
+
+  // component starts here
   return (
     <div className="hook-container use-state-hook-container | text-center flex-col gap-1">
       <h1 className="| fs-heading fw-900">
